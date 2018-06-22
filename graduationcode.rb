72a42d04376f4de533277cc5bc410c96b0212fe9
Hamakyou <38552572+Hamakyou@users.noreply.github.com>
2018-06-22 15:24:48 +0900

    Add files via upload

--- graduationcode.rb
+++ graduationcode.rb
@@ -1,36 +1,66 @@
 #encoding: UTF-8
+
 require 'csv'
+
+print("主専攻を入力して下さい\n")
+syusenkou = gets.chomp
+
 print("csvファイルを入力してください\n")
 filename = gets.chomp
 
 kamoku = Hash.new
-syuutoku = {}
-kamoku = {"sougou1" => /^1[1-3]/, "sougou2"=> /^1[A-C]/, "sougou3" => /^1[D-F]/, }
+borderline = Hash.new
 
- kamoku.each do |key, value|
-    tmp = 0
-    CSV.foreach(filename) do |user|
-        if value =~ user[2]
-            tmp += user[4].to_i
-        end
-        syuutoku[key] = tmp
-    end
+syuutoku = {}
+
+tasenkou = Array.new
+kagaku = "GE6"
+sisutemu = "GE7"
+shigen = "GE8"
+
+if syusenkou == "kagaku"
+    jisenkou = kagaku
+    tasenkou = [sisutemu, shigen]
+elsif syusenkou == "sisutemu" 
+    jisenkou = sisutemu
+    tasenkou = [kagaku, shigen]
+elsif syusenkou == "shigen"
+    jisenkou = shigen
+    tasenkou = [kagaku, sisutemu]
 end
+
+#print(tasenkou, "\n")
+
+kamoku = {"総合I" => /^1[1-3]/, "総合II"=> /^1[A-C]/, "総合III" => /^1[D-F]/, "共通英語" => /^31/,
+"第二外国語" => /^3[2-9]/, "体育" => /^2[1236]/, "必修科目1,2年" => /^GE1/, "選択科目1,2年" => /^(GA1|GE2)/, 
+"自専攻" => /^#{jisenkou}/, "選択科目3,4年" => /^(GE4|GA4|#{tasenkou[0]}|#{tasenkou[1]})/, "必修科目3,4年" => /^GE5/}
+
+borderline = {"総合I" => 2, "総合II"=> 5, "総合III" => 1, "共通英語" => 6,
+"第二外国語" => 3, "体育" => 2, "必修科目1,2年" => 22.5, "選択科目1,2年" => 32, 
+"自専攻" => 20, "選択科目3,4年" => 8, "必修科目3,4年" => 10}
+
+
+kamoku.each do |key, value|
+   tmp = 0
+   CSV.foreach(filename, "r:Shift_JIS:UTF-8") do |user|
+       if value =~ user[2]
+           tmp += user[4].to_f
+       end
+       syuutoku[key] = tmp
+   end
+end
+
 print(syuutoku, "\n")
 
-kagaku = /^GE4/
-system = /^GE5/
-shigen = /^GE6/
+sa = 0
+borderline.each do |key, value|
+	if syuutoku[key] >= value
+		print(key, "はOK\n")
+	else
+		sa = value - syuutoku[key]
+		print(key, "が", sa, "単位分足りません\n")
 
-if syusenkou == kagaku
-    jisenkou = kagaku
-    tasenkou = [system, shigen]
-elsif syusenkou == system 
-    jisenkou = system
-    tasenkou = [kagaku, shigen]
-elsif syusenkou == shigen
-    jisenkou = shigen
-    tasenkou = [kagaku, system]
+	end
 end
 
-#自専攻
+
