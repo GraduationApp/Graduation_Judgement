e9e208fb179c034e0eab15bf9372631c24b89783
濱田恭香 <s1611540@u.tsukuba.ac.jp>
2018-06-27 13:25:32 +0900

    graduationpage

--- test/controllers/graduation_pages_controller_test.rb
+++ test/controllers/graduation_pages_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+class GraduationPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get graduation_pages_home_url
     assert_response :success
   end
 
   test "should get result" do
-    get static_pages_result_url
+    get graduation_pages_result_url
     assert_response :success
   end
 
